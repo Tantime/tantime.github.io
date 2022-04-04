58651c414b50c9282640ac7374aaf5b0f77f3e92
Tantime <48768802+Tantime@users.noreply.github.com>
2022-04-04 02:37:49 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path='/' exact component={Home} />
+                {/* <Route path='/code' exact component={Code} />
+                <Route path='/art' exact component={Art} />
+                <Route path='/about' exact component={About} /> */}
+            </Routes>
+        </Router>
+      </>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from './Button';
+import './Navbar.css';
+
+function Navbar() {
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 768) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    };
+
+    useEffect(() => {
+        showButton()
+    }, []);
+
+    window.addEventListener('resize', showButton);
+
+    return (
+        <>
+            <nav className='navbar'>
+                <div className='navbar-container'>
+                    <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+                        MZT <i className="fab fa-typo3" />
+                    </Link>
+                    <div className='menu-icon' onClick={handleClick}>
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                    </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className='nav-item'>
+                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/code' className='nav-links' onClick={closeMobileMenu}>
+                                Code
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/art' className='nav-links' onClick={closeMobileMenu}>
+                                Art
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/about-me' className='nav-links' onClick={closeMobileMenu}>
+                                About Me
+                            </Link>
+                        </li>
+                    </ul>
+                    {button && <Button buttonStyle='btn--outline' 
+                        onClick={(e) => {
+                            e.preventDefault();
+                            // window.location.href='https://github.com/Tantime';
+                            window.open('https://github.com/Tantime', '_blank');
+                        }}
+                        >GitHub &nbsp;
+                        <i class="fa-brands fa-github"></i>
+                    </Button>}
+                </div>
+            </nav>
+        </>
+    );
+}
+
+export default Navbar
