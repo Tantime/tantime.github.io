af390365de94bc2ab1fc7e77dc9b174572367687
Tantime <48768802+Tantime@users.noreply.github.com>
2022-04-05 14:52:38 -0700

    update

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,7 +7,7 @@ function HeroSection() {
   return (
     <div className='hero-container'>
         <video src='/videos/video-2.mp4' autoPlay loop muted />
-        <h1>ADVENTURE AWAITS</h1>
+        <h1>MATTHEW TAN</h1>
         <p>What are you waiting for?</p>
         <div className='hero-btns'>
             <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>
@@ -21,4 +21,4 @@ function HeroSection() {
   );
 }
 
-export default HeroSection
+export default HeroSection;
