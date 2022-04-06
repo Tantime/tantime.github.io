fa64ca0abd908b147b18ec19ffec90c9967267e2
Tantime <48768802+Tantime@users.noreply.github.com>
2022-04-05 17:17:22 -0700

    updated page and card images

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,17 +5,17 @@ import './Cards.css';
 function Cards() {
   return (
     <div className='cards'>
-        <h1>Check out these EPIC Destinations</h1>
+        <h1>Check out my projects!</h1>
         <div className='cards__container'>
             <div className='cards__wrapper'>
                 <ul className='cards__items'>
                     <CardItem 
-                    src='images/img9.jpg' 
+                    src='images/img-9.jpg' 
                     text='Explore the hidden waterfall deep inside the Amazon Jungle'
                     label='Adventure'
                     path='/ios-dev' />
                     <CardItem 
-                    src='images/img2.jpg' 
+                    src='images/img-2.jpg' 
                     text='Travel through the Islands of Bali on a Private Cruise'
                     label='Luxury'
                     path='/hydrate' />
--- src/components/pages/Code.js
+++ src/components/pages/Code.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import '../../App.css';
+import Cards from '../Cards';
 
 export default function Code() {
-    return <h1 className='code'>CODE</h1>;
+    return(
+        <>
+            <h1 className='code'>CODE</h1>
+            <Cards />
+        </>
+    );
 }
