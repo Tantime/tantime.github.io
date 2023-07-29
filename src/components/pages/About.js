394070b9f2a8608e2f3259b3b7f0974dded7e66d
Tantime <48768802+Tantime@users.noreply.github.com>
2023-07-28 18:11:24 -0700

    Added: about page content, Changes: reverted path url (broke it last commit) to normal -- note: reload on pages other than home still cause 404 error

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,5 +1,6 @@
-import React, {useEffect} from 'react';
-import '../../App.css';
+import React, {useEffect} from 'react'
+import '../../App.css'
+import './About.css'
 
 export default function About() {
   useEffect(() => {
@@ -11,9 +12,20 @@ export default function About() {
         <div className='about'>
           <h1>ABOUT</h1>
         </div>
+        <div className='container-intro'>
           <div className='image-cropper'>
               <img src='/images/pfp3.jpg' alt='matthew pfp'/>
           </div>
+          <p className='intro-text'>
+            Hi, I'm Matthew! I'm currently a junior studying Computer Engineering at 
+            UC San Diego who loves cooking and playing volleyball. I'm particularly
+            interested in working in software engineering or embedded systems.
+            I'm always eager to learn and am always open to connecting!
+            <br/><br/>
+            Contact: matthew.z.tan@gmail.com
+          </p>
+        </div>
+        <img className='gif-about' src='videos/ghibli4.gif' alt=''/>
       </>
-  );
+  )
 }
