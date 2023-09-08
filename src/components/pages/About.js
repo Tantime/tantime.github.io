970758cf44ca0ffc952a3554ebbf1b24c6a42688
Tantime <48768802+Tantime@users.noreply.github.com>
2023-09-08 15:19:31 -0700

    Updated about blurb, added icons with hover animation to GitHub and LinkedIn links, removed gif

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -19,25 +19,32 @@ export default function About() {
           </div>
           <p className='intro-text'>
             Hi, I'm Matthew! I'm currently a junior studying Computer Engineering at 
-            UC San Diego who loves cooking and playing volleyball. I'm particularly
-            interested in working in software engineering or embedded systems.
-            I'm always eager to learn and open to connecting!
+            UC San Diego who loves cooking and playing volleyball. I have past 
+            technical internship experience in both frontend web development and 
+            QA automation software testing. I'm particularly interested in working 
+            in software engineering and embedded systems. I'm always eager to learn 
+            and open to connecting, so don't hesitate to reach out!
             <br/><br/>
             Contact: matthew.z.tan@gmail.com
           </p>
         </div>
-        <div className='about-extra'>
-          <img className='gif-about' src='videos/ghibli4.gif' alt=''/>
-          <p id='about-gif-description'><small style={{ fontStyle:'italic' }}>The Secret World of Arriety by Hayao Miyazaki</small></p>
-        </div>
-        <br/><br/>
+        
         <div className='link-about'>
           <div className='lab-links-newtab'>
-            <a className='link-about' rel='noreferrer' target='_blank' href='https://github.com/tantime'>GitHub</a>
+            <a className='link-about' rel='noreferrer' target='_blank' href='https://github.com/tantime'>
+              <i class="fa-brands fa-github fa-flip about-link-animation"></i> GitHub
+            </a>
             {/* <a className='link-about' rel='noreferrer' target='_blank' href='https://www.instagram.com/tantime.jpg/'>Instagram</a> */}
-            <a className='link-about' rel='noreferrer' target='_blank' href='https://www.linkedin.com/in/matthewztan'>LinkedIn</a>
+            <a className='link-about' rel='noreferrer' target='_blank' href='https://www.linkedin.com/in/matthewztan'>
+              <i class="fa-brands fa-linkedin-in fa-flip about-link-animation"></i> LinkedIn
+            </a>
           </div>
         </div>
+        {/* <div className='about-extra'>
+          <img className='gif-about' src='videos/ghibli4.gif' alt=''/>
+          <p id='about-gif-description'><small style={{ fontStyle:'italic' }}>The Secret World of Arriety by Hayao Miyazaki</small></p>
+        </div> */}
+        <br/><br/>
       </div>
     </>
   )
