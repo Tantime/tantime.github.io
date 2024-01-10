38b2cdaf0549a82a9247b70783774767d6cbf3a8
Matthew Tan <48768802+Tantime@users.noreply.github.com>
2024-01-09 16:24:39 -0800

    Added Lab Report titles

--- src/components/pages/Cse15Labs.js
+++ src/components/pages/Cse15Labs.js
@@ -14,11 +14,12 @@ export default function Cse15Labs() {
         <h1>CSE15L Lab Reports</h1>
         <p className='p-cse'><em>Author: Matthew Tan</em></p>
         <div className='lab-links'>
-          <Link to='/code/cse15labs/lab1'>Lab Report 1 - Week 2</Link><br/>
-          <Link to='/code/cse15labs/lab2'>Lab Report 2 - Week 4</Link><br/>
-          <Link to='/code/cse15labs/lab3'>Lab Report 3 - Week 6</Link><br/>
-          <Link to='/code/cse15labs/lab4'>Lab Report 4 - Week 8</Link><br/>
-          <Link to='/code/cse15labs/lab5'>Lab Report 5 - Week 10</Link><br/>
+          <Link to='/code/cse15labs/lab1'>Lab Report 1 - Remote Access and the Filesystem</Link><br/>
+          <Link to='/code/cse15labs/lab2'>Lab Report 2 - Incremental Programming and Debugging
+</Link><br/>
+          <Link to='/code/cse15labs/lab3'>Lab Report 3 - Setup GitHub Access from ieng6</Link><br/>
+          <Link to='/code/cse15labs/lab4'>Lab Report 4 - Additional Testers for markdown-parse</Link><br/>
+          <Link to='/code/cse15labs/lab5'>Lab Report 5 - Analyzing Different Outputs</Link><br/>
         </div>
       </div>
     </>
